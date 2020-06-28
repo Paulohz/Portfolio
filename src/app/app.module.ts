f9050c3a8f33c1020f519f20fc765335b7463c71
paulohz <pauloh.alvesdacosta@gmail.com>
2020-06-27 23:18:21 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from  '@angular/material/list';
+import { MatCardModule } from  '@angular/material/card';
+
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EducationComponent } from './education/education.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    AboutmeComponent,
+    PortfolioComponent,
+    ContactsComponent,
+    EducationComponent
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
